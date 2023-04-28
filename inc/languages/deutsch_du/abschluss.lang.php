c425a44a6c91b11a57378456a7e7b30e9799aa40
saen91 <69750696+saen91@users.noreply.github.com>
2023-04-28 14:56:11 +0200

    Update abschluss.lang.php

--- inc/languages/deutsch_du/abschluss.lang.php
+++ inc/languages/deutsch_du/abschluss.lang.php
@@ -3,5 +3,23 @@
 //ALLGEMEINES
 $l['abschluss_name'] = "Abschlussjahrgang berechnen"; 
 $l['abschluss_welcome'] = "Herzlich Willkommen in der automatischen Einschulung- & Abschlussberechnung.<br>Bitte fülle folgendes Formular aus. Ansonsten könnten falsche Daten angegeben werden."; 
+$l['abschluss_name_button']"Einschulung & Abschluss berechnen";
 
+// Hauptseite
+$l['abschluss_main_gebi'] ="Angegebener Geburtstag:";
+$l['abschluss_main_schule'] ="Ausgewählte Schule:";
+$l['abschluss_main_alter'] ="Reguläres Einschulalter:";
+$l['abschluss_main_jahre'] ="Pflicht-Schuljahre:";
+$l['abschluss_main_rein'] ="Einschulung:";
+$l['abschluss_main_raus'] ="Abschluss:";
+
+// Schulübersicht
+$l['abschluss_uebersicht'] = "Schulübersicht"; 
+
+$l['abschluss_schule_rein'] = "Einschulungsalter";
+$l['abschluss_schule_jahre'] = "Jahrgänge";
+$l['abschluss_schule_monat'] = "Einschulung bis Geburtsmonat";
+$l['abschluss_schule_wo'] = "Standort der Schule";
+$l['abschluss_schule_alter'] = "Jahre";
+$l['abschluss_schule_desc']'] = "<b>Beschreibung der Schule</b>";
 ?>
