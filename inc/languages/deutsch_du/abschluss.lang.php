a4badddd658af793171d7654018702c471ba2214
saen91 <69750696+saen91@users.noreply.github.com>
2023-04-28 13:56:03 +0200

    Create abschluss.lang.php

---
+++ inc/languages/deutsch_du/abschluss.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+//ALLGEMEINES
+$l['abschluss_name'] = "Abschlussjahrgang berechnen"; 
+$l['abschluss_welcome'] = "Herzlich Willkommen in der automatischen Einschulung- & Abschlussberechnung.<br>Bitte fülle folgendes Formular aus. Ansonsten könnten falsche Daten angegeben werden."; 
+
+?>
