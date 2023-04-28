ca1e7fca428ab70d8819a202a67988a74e8baaa4
saen91 <69750696+saen91@users.noreply.github.com>
2023-04-28 13:54:45 +0200

    Create abschluss.lang.php

---
+++ inc/languages/deutsch_du/admin/abschluss.lang.php
@@ -0,0 +1,56 @@
+<?php
+//Beschreibung im ACP vom Plugin
+$l['abschluss_name'] = "Abschluss berechnen"; 
+$l['abschluss_desc_acp'] = "Abschluss automatisch berechnen."; 
+
+//Schule hinzufügen, editieren & löschen
+$l['abschluss_manage'] = "Schulen verwalten"; 
+$l['abschluss_overview'] = "Übersicht der Schulen"; 
+$l['abschluss_add_entry'] = "Schule hinzufügen";
+$l['abschluss_add_entry_desc'] = "Neue Schule hinzufügen.";
+$l['abschluss_add_entry_solved'] = "Eine neue Schule wurde hinzugefügt.";
+$l['abschluss_edit_entry'] = "Schule bearbeiten.";
+$l['abschluss_edit_entry_desc'] = "Hier bearbeitest du eine Schule.";
+$l['abschluss_edit_entry_solved'] = "Die Schule wurde bearbeitet.";
+$l['abschluss_delete_entry_solved'] = "Die Schule wurde gelöscht.";
+$l['abschluss_delete_entry_question'] = "Möchtest du diese Schule wirklich löschen?";
+
+//Allgemeines
+$l['abschluss_overview_entries'] ="Übersicht"; 
+$l['abschluss_overview_entries_desc'] = "Eine Übersicht aller eingetragener Schulen.";
+$l['abschluss_options'] = "Optionen";
+$l['abschluss_edit'] = "Editieren";
+$l['abschluss_delete'] = "Löschen";
+$l['abschluss_add'] = "Hinzufügen";
+$l['abschluss_send'] = "Absenden";
+
+//Überschriften Overview Seite
+$l['abschluss_overview_titel_titel'] = "Schulen";
+$l['abschluss_overview_titel_name'] = "Name der Schule";
+$l['abschluss_overview_titel_desc'] = "Beschreibung";
+$l['abschluss_overview_titel_alter'] = "Einschulungsalter";
+$l['abschluss_overview_titel_monat'] = "Ab welchem Monat (Zahl) wird man ein Jahr später eingeschult?";
+$l['abschluss_overview_titel_jahre'] = "Jahrgänge";
+$l['abschluss_overview_titel_standort'] = "Wo befindet sich die Schule?";
+
+//Formular
+$l['abschluss_form_name'] = "Schulname";
+$l['abschluss_form_name_desc'] = "Wie heißt die Schule?";
+$l['abschluss_form_desc'] = "Beschreibung";
+$l['abschluss_form_desc_desc'] = "Beschreibe hier die Schule (freiwillig).";
+$l['abschluss_form_alter'] = "Einschulalter";
+$l['abschluss_form_alter_desc'] = "Ab wann wird man eingeschult?";
+$l['abschluss_form_jahre'] = "Jahrgänge";
+$l['abschluss_form_jahre_desc'] = "Wie viele Jahre besucht man die Schule (nur Zahl)?";
+$l['abschluss_form_monate'] = "Ab welchem Monat (Zahl) wird man ein Jahr später eingeschult?";
+$l['abschluss_form_monate_desc'] = "Bitte nur die Zahl eintragen";
+$l['abschluss_form_standort'] = "Wo befindet sich die Schule?";
+$l['abschluss_form_standort_desc'] = "Wo genau befindet sich die Schule?";
+
+//errors
+$l['abschluss_error_titel'] = "Kein Schulname angelegt!";
+$l['abschluss_error_alter'] = "Kein Einschulalter eingetragen!";
+$l['abschluss_error_jahre'] = "Keine Angabe zu den Jahrgängen!";
+$l['abschluss_error_monate'] = "Keine Angabe zu Monat!";
+$l['abschluss_error_standort'] = "Keine Angabe zum Standort der Schule!";
+?>
