62eb12b21ecdb0824e53391c087c8dfc09c41a24
saen91 <69750696+saen91@users.noreply.github.com>
2024-02-01 19:31:43 +0100

    Update abschluss.lang.php

--- inc/languages/deutsch_du/abschluss.lang.php
+++ inc/languages/deutsch_du/abschluss.lang.php
@@ -1,25 +1,41 @@
 <?php
+// Hauptseite php
+$l['schoolview'] = "Zur Schulübersicht";
+$l['navi']="Abschluss & Einschulung berechnen";
+$l['location'] = "Location";
+$l['noperson'] = "Es waren keine Personen in deinem Jahrgang.";
+$l['abschluss_error'] = "<center>Bitte trage ein Geburtstag ein und wähle die Schule aus.</center>";
 
-//ALLGEMEINES
-$l['abschluss_name'] = "Abschlussjahrgang berechnen"; 
-$l['abschluss_welcome'] = "Herzlich Willkommen in der automatischen Einschulung- & Abschlussberechnung.<br>Bitte fülle folgendes Formular aus. Ansonsten könnten falsche Daten angegeben werden."; 
-$l['abschluss_name_button']"Einschulung & Abschluss berechnen";
+//abschluss_main tpl
+$l['titeltpl'] = "Abschlussberechen";
+$l['abschluss_welcome'] = "Willkommen bei der Automatischen Berechnung des Einschulungs- und Abschlussjahres. Gebe hier einfach das Geburtsdatum deines Charakters an und lassse dich überraschen";
 
-// Hauptseite
-$l['abschluss_main_gebi'] ="Angegebener Geburtstag:";
-$l['abschluss_main_schule'] ="Ausgewählte Schule:";
-$l['abschluss_main_alter'] ="Reguläres Einschulalter:";
-$l['abschluss_main_jahre'] ="Pflicht-Schuljahre:";
-$l['abschluss_main_rein'] ="Einschulung:";
-$l['abschluss_main_raus'] ="Abschluss:";
+//abschluss_berechnet tpl
+$l['ergebnis'] = "Deine Ergebnisse";
+$l['gebi'] = "Angegebener Geburtstag";
+$l['school'] = "Ausgewählte Schule";
+$l['einschulalter'] = "Einschulalter";
+$l['schuljahre'] = "Anzahl der Schuljahre";
+$l['schoolinfo'] = "Informationen zum Schulabschluss";
+$l['einschulung'] = "Deine Einschulung";
+$l['abschluss'] = "Dein Abschluss";
+$l['andere'] = "Ebenfalls einen Abschluss in diesem Jahrgang & dieser Schule hatten...";
 
-// Schulübersicht
-$l['abschluss_uebersicht'] = "Schulübersicht"; 
+//Schulübersicht php
+$l['navi_school'] ="Schulübersicht";
 
-$l['abschluss_schule_rein'] = "Einschulungsalter";
-$l['abschluss_schule_jahre'] = "Jahrgänge";
-$l['abschluss_schule_monat'] = "Einschulung bis Geburtsmonat";
-$l['abschluss_schule_wo'] = "Standort der Schule";
-$l['abschluss_schule_alter'] = "Jahre";
-$l['abschluss_schule_desc']'] = "<b>Beschreibung der Schule</b>";
+//abschluss_schule tpl
+$l['schultitel'] = "Schulübersicht";
+$l['schuldesc'] ="Hier findet ihr eine Übersicht über unsere aktuell hinterlegten Schulen. Ihr bekommt Informationen zum <i>Standort</i> (das Bild vorne stellt den Kontinent dar!), <i>Einzugsgebiet</i> der Schule <i>Einschulalter</i>, <i>Anzahl der Schuljahre</i> und den <i>Stichmonat</i> der Einschulung - dein Charakter muss also vorher Geburtstag habe.";
+$l['allkonti'] = "Alle Kontinente";
+$l['kontiaus'] ="Kontinent auswählen:";
+
+//abschluss_schulview tpl
+$l['location'] = "Standort";
+$l['stich'] = "Stichmonat";
+$l['einzug'] = "Einzugsgebiet";
+$l['schuldesc2'] = "Schulbeschreibung"
+
+
+
 ?>
